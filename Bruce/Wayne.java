9bc0ad183717f9ab6cdbdf8308a74437305ed95e
user <batman@gmail.com>
2017-06-07 16:28:44 +0530

    Batman Begins

---
+++ Bruce/Wayne.java
@@ -0,0 +1,11 @@
+package Bruce;
+
+public class Wayne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'am not the hero Gotham deserves, but the hero Gotham needs!!");
+
+	}
+
+}
